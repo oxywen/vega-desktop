44c3e1a8e5b3da1cc458e885f6d8f6643634fedb
Wen <oxywen11@gmail.com>
2023-12-03 01:35:05 +0800

    chore: add style lint

---
+++ .stylelintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: ['stylelint-prettier'],
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-recommended-less',
+    'stylelint-config-recommended-vue',
+    'stylelint-config-recess-order',
+    'stylelint-config-prettier',
+    'stylelint-prettier/recommended'
+  ],
+  rules: {
+    'prettier/prettier': true
+  }
+}
