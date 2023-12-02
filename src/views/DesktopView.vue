1c0dabfae3b5b951303838d6e3bd9426fc24dd10
Wen <oxywen11@gmail.com>
2023-12-03 01:35:29 +0800

    feat: init desktop view

---
+++ src/views/DesktopView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="vega-desktop"></div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="less">
+.vega-desktop {
+  width: 100%;
+  height: 100%;
+  min-width: 800px;
+  min-height: 600px;
+  overflow: hidden;
+  font-size: 16px;
+  background-image: url('@/assets/wallpaper/fresh girl.jpg');
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  background-color: rgb(115, 141, 112);
+}
+</style>
